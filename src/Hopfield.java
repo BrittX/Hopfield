118387fbcdc27111488a3653231b4811d880e302
BrittX <brittxbarnes@gmail.com>
2017-04-15 15:45:20 -0700

    readIns now stores training pairs as matrices
    Updated function readIns to store the training samples as a 2D matrix.
    This way I won't have to do it laterz. I need to update what the
    function itself returns since I'md oing everything in this function. Now
    I can work on transposing the matrices

--- src/Hopfield.java
+++ src/Hopfield.java
@@ -18,9 +18,9 @@ public class Hopfield {
             List<Integer> trains = readIns(fName);
             dimensions = trains.get(0); //store first value as number of dimensions
             pairs = trains.get(1); //second value as number of pairs
-            cols = trains.get(2);
-            rows = trains.get(3);
-            tSamples = trains.subList(2, trains.size()); //store the remainder which are our training samples
+//            cols = trains.get(2);
+//            rows = trains.get(3);
+//            tSamples = trains.subList(2, trains.size()); //store the remainder which are our training samples
         }
         catch (NullPointerException e) {
             System.out.println("\nYou entered an invalid file format");
@@ -29,6 +29,77 @@ public class Hopfield {
         }
     }
 
+    //Function to read testing file and store contents in int array
+    private List<Integer> readIns(String inFile) {
+        List<Integer> inVals = new ArrayList<Integer>();
+
+        try {
+            int count = 0, rows = 0, cols = 0, check = 0, curr = 0;
+            BufferedReader br = new BufferedReader(new FileReader((inFile)));
+            String lines;
+            while((lines = br.readLine()) != null) {
+                //Reading and storing training inputs
+                if (count > 1) {
+                    //Check if line itself is new line
+                    if (lines.equals("")) {
+                        check += 1;
+                        if (check == 2) {
+                            break;
+                        }
+                        continue;
+                    }
+                    rows+=1; //increment number of rows
+                    cols = lines.length(); //store number of columns in each training pair
+                }
+                else {
+                    //Reading first two lines so just store in array
+                    inVals.add(Integer.parseInt(lines));
+                }
+                count++; //increment count
+            }
+            br.close();
+            BufferedReader brr = new BufferedReader(new FileReader(inFile));
+            //now read and store in 2D matrix
+            int[][][] samples = new int[inVals.get(1)][rows][cols];
+            int index = 0;
+            while((lines = brr.readLine()) != null) {
+                if (curr > 2) {
+                    if (lines.equals("")) {
+                        index++;
+                    }
+                        for (int j = 0; j < rows; j++) {
+                            for (int k = 0; k < lines.length(); k++) {
+                                //Cycle through each character in line
+                                if (lines.charAt(k) == 'O' || lines.charAt(k) == '0') {
+                                    samples[index][j][k] = 1;
+                                }
+                                else if (lines.charAt(k) == ' ') {
+                                    samples[index][j][k] = -1;
+                                }
+                            }
+                        }
+                }
+                curr++;
+            }
+            brr.close();
+            return inVals;
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("\nFile inputted is invalid");
+            e.printStackTrace();
+            return null;
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println("\nFile is not of correct format");
+            e.printStackTrace();
+            return null;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    /*
     //Function to get weight matrix
     public double[][] weightMatrix(List<Integer> samples) {
         double [][]weightSamples = new double[this.pairs][this.dimensions];
@@ -46,10 +117,9 @@ public class Hopfield {
                 }
             }
         }
-        //System.out.println(weightSamples);
         return weightSamples;
-
     }
+    */
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -73,10 +143,8 @@ public class Hopfield {
                     System.out.print(">>> ");
                     String trainFile = scan.next();
                     Hopfield hopfield = new Hopfield(trainFile);
-                    hopfield.weightMatrix(hopfield.tSamples);
-                    //System.out.println(trainIns);
+                    //hopfield.weightMatrix(hopfield.tSamples);
                 }
-
                 if (choice == 3) {
                     System.out.println("Goodbye!\n");
                     quit = true;
@@ -91,63 +159,5 @@ public class Hopfield {
         System.exit(1);
     }
 
-    //Function to read testing file and store contents in int array
-    private List<Integer> readIns(String inFile) {
-        List<Integer> inVals = new ArrayList<Integer>();
 
-        try {
-            int count = 0, rows = 0, cols = 0;
-            BufferedReader br = new BufferedReader(new FileReader((inFile)));
-            String lines;
-            while((lines = br.readLine()) != null) {
-                //Reading and storing training inputs
-                if (count > 1) {
-                    //Check if line itself is new line
-                    if (lines.equals("")) {
-                        //System.out.println("num rows: " + rows);
-                        continue;
-                    }
-                    rows+=1; //increment number of rows
-                    for (int i = 0; i < lines.length(); i++) {
-                        //keep track of dimensions of matrix
-                        if (i == lines.length() - 1) {
-                            cols = i;
-                            //System.out.println(cols);
-                        }
-                        //Cycle through each character in line
-                        if (lines.charAt(i) == 'O' || lines.charAt(i) == '0') {
-                            inVals.add(1);
-                        }
-                        if (lines.charAt(i) == ' ') {
-                            inVals.add(-1);
-                        }
-                    }
-                }
-                else {
-                    //Reading first two lines so just store in array
-                    inVals.add(Integer.parseInt(lines));
-                }
-                count++; //increment count
-            }
-            br.close();
-            inVals.add(2, cols); //add number of columns
-            inVals.add(3, (rows/5)-1); //add number of rows
-            System.out.println(inVals);
-            return inVals;
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("\nFile inputted is invalid");
-            e.printStackTrace();
-            return null;
-        }
-        catch (IndexOutOfBoundsException e) {
-            System.out.println("\nFile is not of correct format");
-            e.printStackTrace();
-            return null;
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
 }
